86d0f604a4cb78bea085675360981aa8199aa7c3
Jorge Paiva <jorgepaiva1806@gmail.com>
2025-04-09 17:59:24 -0400

    feat: cria os filtros de servidor

--- src/main/java/servidores/api/service/ServidorEfetivoService.java
+++ src/main/java/servidores/api/service/ServidorEfetivoService.java
@@ -74,41 +74,41 @@ public class ServidorEfetivoService {
                 }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    // public ServidorEfetivo findById(Long id) {
-    //     return repository.findById(id)
-    //             .orElseThrow(() -> new RecordNotFoundException(id));
-    // }
+    public ServidorEfetivo findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id));
+    }
 
-    // public List<ServidorEfetivo> filtrarPorUnidade(@Param("unidId") Long id) {
-    //     return repository.findByPessoaLotacoesUnidadeId(id);
-    // }
+    public List<ServidorEfetivo> filtrarPorUnidade(@Param("unidId") Long id) {
+        return repository.findByPessoaLotacoesUnidadeId(id);
+    }
 
-    // public ResponseEntity<String> uploadFoto(Long id, MultipartFile file) {
-    //     ServidorEfetivo servidor = repository.findById(id).orElseThrow();
-    //     String nomeUnico = UUID.randomUUID() + "_" + file.getOriginalFilename();
-    //     String url = minioService.uploadFoto(file, nomeUnico);
+    public ResponseEntity<String> uploadFoto(Long id, MultipartFile file) {
+        ServidorEfetivo servidor = repository.findById(id).orElseThrow();
+        String nomeUnico = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String url = minioService.uploadFoto(file, nomeUnico);
 
-    //     FotoPessoa foto = new FotoPessoa();
-    //     foto.setPessoa(servidor.getPessoa());
-    //     foto.setFpBucket("imagens");
-    //     foto.setFpHash(nomeUnico);
-    //     foto.setFpData(LocalDate.now());
-    //     fotoRepository.save(foto);
+        FotoPessoa foto = new FotoPessoa();
+        foto.setPessoa(servidor.getPessoa());
+        foto.setFpBucket("imagens");
+        foto.setFpHash(nomeUnico);
+        foto.setFpData(LocalDate.now());
+        fotoRepository.save(foto);
 
-    //     return ResponseEntity.ok(url);
-    // }
+        return ResponseEntity.ok(url);
+    }
 
-    // public ResponseEntity<String> getFoto(Long id) {
-    //     FotoPessoa foto = fotoRepository.findByPessoaId(id)
-    //             .orElseThrow(() -> new RecordNotFoundException(id));
+    public ResponseEntity<String> getFoto(Long id) {
+        FotoPessoa foto = fotoRepository.findByPessoaId(id)
+                .orElseThrow(() -> new RecordNotFoundException(id));
 
-    //     String url = minioService.getUrlTemporaria(foto.getFpHash());
-    //     return ResponseEntity.ok(url);
-    // }
+        String url = minioService.getUrlTemporaria(foto.getFpHash());
+        return ResponseEntity.ok(url);
+    }
 
-    // public void delete(Long id) {
+    public void delete(Long id) {
 
-    //     repository.delete(repository.findById(id)
-    //             .orElseThrow(() -> new RecordNotFoundException(id)));
-    // }
+        repository.delete(repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id)));
+    }
 }
